aa71919887bc179e26ee9a37e09456606c922282
Ancientshield <jacklin2100@gmail.com>
2023-06-04 21:34:29 +0800

    add $refs greering()

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const app = Vue.createApp({
-  data: () => ({
-    title: 'Hello',
-  }),
-}).mount('#app')
+	data: () => ({
+		title: 'Hello',
+	}),
+}).mount('#app');
+
+app.$refs.greeting.innerText = 'Another Hello';
